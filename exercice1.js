13a200f4bdfa3c0d5b8a5a0b885b08a04c858ef0
zvrsd <65198569+zvrsd@users.noreply.github.com>
2021-01-18 13:00:31 +0100

    Exercice 1

---
+++ exercice1.js
@@ -0,0 +1,9 @@
+//var annee = 2016;
+
+var annee = prompt("Entrez une année");
+
+if ((annee % 4 === 0 && annee %100 > 0) || annee % 400 === 0) {
+	console.log(annee +" est bissextile");
+}else{
+	console.log(annee +" n'est pas bissextile");
+}
