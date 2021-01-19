31427ee3167f3c004a0480f6dce63d37588577a8
Moussa Camara <65198569+zvrsd@users.noreply.github.com>
2021-01-19 16:38:16 +0100

    modification

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-var bonjour = function () {
-    console.log("variable");
-}
+var id = document.getElementById('test');
+console.log( id );
+
+console.log(document.getElementsByClassName('container'));
 
 
-var additionner =function (chiffre1,chiffre2) {
- 
-    return chiffre1+chiffre2;
-}
+console.log(document.getElementsByTagName('p'));
 
-
-function addition(chiffre1,chiffre2) {
-   
-    return chiffre1+chiffre2;
-}
-//console.log(addition(6,4));
-
-function soustraction(chiffre1,chiffre2) {
-   
-    return chiffre1-chiffre2;
-}
-
-function division(chiffre1,chiffre2) {
-   
-    return chiffre1/chiffre2;
-}
-
-function multiplication(chiffre1,chiffre2) {
-   
-    return chiffre1*chiffre2;
-}
-
-function modulo(chiffre1,chiffre2) {
-   
-    return chiffre1%chiffre2;
-}
+console.log( document.querySelector('#test') );
+console.log( document.querySelectorAll('.container') );
+console.log( document.querySelectorAll('p') );
