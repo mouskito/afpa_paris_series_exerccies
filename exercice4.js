d52763e51a5fe4a05517c830aa7ac4bc528140e2
Moussa Camara <65198569+zvrsd@users.noreply.github.com>
2021-01-19 09:32:43 +0100

    exercice4

---
+++ exercice4.js
@@ -0,0 +1,16 @@
+var fruits=['Banane','Raisin','Pommes'];
+
+document.write(fruits);
+console.log(fruits.join());
+
+/* Je demande à l'utilisateur de saisir un fruit avec prompt */
+var saisi_utilisateur = prompt('Saisir un fruit');
+for(var index=0; index < fruits.length; index++){
+	//console.log("test");
+
+	if(saisi_utilisateur ==  fruits[index]){
+		console.log("Vous avez mangé " +saisi_utilisateur);
+		fruits.splice(index, 1);
+	}
+}
+console.log(fruits);
