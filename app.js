4620b7a18e7b9a69ebdd02a5c60c088a01d71381
Moussa Camara <moussa.camara@iknsa.com>
2021-01-20 15:40:48 +0100

    correction calcul

--- app.js
+++ app.js
@@ -1,21 +1,92 @@
-//var para = document.getElementsByTagName('p');
-var para = document.querySelectorAll('p');
+/*var mes_classes = document.querySelector('.container').classList;
 
-console.log(para);
+mes_classes.add("nouvelle");
 
-for(var index=0; index<para.length; index++){
-	if (index%2==0) {
-		//para[index].style.background='red';
-		//para[index].style.color='blue';
+mes_classes.remove("vanessa");
+console.log(mes_classes);
 
-		// Css Multiple
-		para[index].style.cssText = 'background:orange; color:blue; width:250px; heigth:150px;'
+// toggle
+
+window.setInterval(function () {
+	mes_classes.toggle("nouvelle_classe")
+},1000)
+
+
+/**Les evenements 
+var p = document.querySelector('p');
+p.onclick=function () {
+	alert('Tu viens de cliquer');
+}
+*/
+
+function jeClique() {
+	alert("C'est bon je viens de cliquer!")
+}
+
+function jeDoubleClique() {
+	alert("C'est bon je viens de double cliquer!")
+}
+
+
+var souris = document.querySelector('.test');
+console.log(souris);
+souris.onmouseover=function () {
+	souris.style.background='red';
+}
+
+
+souris.onmouseout=function () {
+	souris.style.background='white';
+}
+
+
+/** Correction calcul ***/
+
+var nombre1 = document.getElementById('nombre1');
+var nombre2 = document.getElementById('nombre2');
+var operateur = document.getElementById('operateur');
+
+document.getElementById('calculer').onclick = function () {
+	if (operateur.value == "+") {
+		
+		//console.log(parseInt(nombre1.value)+parseInt(nombre2.value));
+		
+		alert(nombre1.value+""+operateur.value+""+nombre2.value+"="+
+			(parseInt(nombre1.value)+parseInt(nombre2.value)));
 	}
-	else{
-
-		para[index].style.background='yellow';
+	else if (operateur.value == "-") {
+		
+		//console.log(parseInt(nombre1.value)+parseInt(nombre2.value));
+		
+		alert(nombre1.value+""+operateur.value+""+nombre2.value+"="+
+			(parseInt(nombre1.value)-parseInt(nombre2.value)));
+	}else if (operateur.value == "*") {
+		
+		//console.log(parseInt(nombre1.value)+parseInt(nombre2.value));
+		
+		alert(nombre1.value+""+operateur.value+""+nombre2.value+"="+
+			(parseInt(nombre1.value)*parseInt(nombre2.value)));
 	}
 }
+
+
+/******/
+
+
+
+
+
+/*
+var nom = document.getElementById('nom');
+
+console.log(nom);
+
+nom.onkeyup= function () {
+	console.log(nom.value)
+}
+*/
+
+
 
 
 
