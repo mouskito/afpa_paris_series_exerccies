c8018979aa6d671f18dff00103c4d82b3e88a0fb
Moussa Camara <moussa.camara@iknsa.com>
2021-01-21 16:28:43 +0100

    ajout addEventListener

--- form.js
+++ form.js
@@ -29,7 +29,7 @@ function verif() {
 			error++;
 		}else{
 			tous_les_inputs_type_text[index].style.background = 'red';
-			
+
 		}
 	}
 
@@ -37,9 +37,28 @@ console.log(error);
 	if(error>0){	
 		return false;
 	}
-
-
 }
 /*document.querySelector('#submit').onclick=function () {
 	alert("OK")
 }*/
+
+function change() {
+	ma_classe_test.style.background='red';
+}
+function delete_supprime() {
+	ma_classe_test.style.background='green';
+}
+
+var ma_classe_test = document.querySelector('.test');
+
+ma_classe_test.addEventListener('mouseover', change)
+ma_classe_test.addEventListener('mouseout', delete_supprime)
+
+var tel = document.querySelector('#tel');
+
+tel.addEventListener('keyup', function () {
+
+	if (isNaN(tel.value)) {
+		document.querySelector('.error').innerHTML='<p style="color:red">Pas valide </p>';
+	}
+})
