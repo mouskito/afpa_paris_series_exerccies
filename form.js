ad4ec0296bfdc5968a5fa483689351db7d5e7896
Moussa Camara <moussa.camara@iknsa.com>
2021-01-21 16:42:28 +0100

    ajout regex sur le tel

--- form.js
+++ form.js
@@ -28,9 +28,20 @@ function verif() {
 
 			error++;
 		}else{
-			tous_les_inputs_type_text[index].style.background = 'red';
+			tous_les_inputs_type_text[index].style.background = 'white';
 
 		}
+	}
+
+	//var mobile =  /^(?:(?:+|00)33[\s.-]{0,3}(?:(0)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/gm;
+
+	var mobile = /^(01|02|03|04|05|06|07|08|09)[0-9]{8}$/;
+	
+	if (!mobile.test(document.querySelector('#tel').value)) {
+		alert('Numero KO')
+		document.querySelector('#tel').focus();
+		document.querySelector('#tel').style.background='red';
+		return false;
 	}
 
 console.log(error);
@@ -60,5 +71,9 @@ tel.addEventListener('keyup', function () {
 
 	if (isNaN(tel.value)) {
 		document.querySelector('.error').innerHTML='<p style="color:red">Pas valide </p>';
+	}
+	else{
+
+		document.querySelector('.error').innerHTML='';
 	}
 })
